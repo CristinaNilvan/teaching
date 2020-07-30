60b0aee88cb61626939f479b060bd9fb3136604d
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-07-30 23:01:24 +0300

    teaching clean code

---
+++ materials/clean_code/simple_example/C++/NON_OOP/good_example/clean_code_good_sample.cpp
@@ -0,0 +1,38 @@
+/*
+  INFO: Simple clean code example in C++ without TDD or any exception handler.
+  SCOPE: Learning process.
+*/
+
+#include <iostream>
+
+int getFibonacciNumber (int positionOfNumber) {
+
+  if (positionOfNumber == 1) return 1;
+  if (positionOfNumber == 2) return 1;
+
+  return getFibonacciNumber(positionOfNumber - 1) + getFibonacciNumber(positionOfNumber - 2);
+}
+
+int getFactorialNumber (int positionOfNumber) {
+
+  if (positionOfNumber == 0) return 1;
+  if (positionOfNumber == 1) return 1;
+
+  return positionOfNumber * getFactorialNumber(positionOfNumber - 1);
+}
+
+void assertions () {
+
+  if (getFibonacciNumber(7) == 13)
+    std::cout << "Fibonacci test passed;\n";
+
+  if (getFactorialNumber(3) == 6)
+    std::cout << "Factorial test passed;\n";
+}
+
+int main(int argc, char const *argv[]) {
+
+  assertions ();
+  
+  return 0;
+}
